72ced7c883776901f7f9ba354284f80ad1fe0756
rahuja123 <r.ahuja123@gmail.com>
2018-04-15 17:54:21 +0530

    final

--- ubuntu_webcam.py
+++ ubuntu_webcam.py
@@ -1,22 +1,18 @@
 import cv2
-import time
 
-def show_webcam(mirror=False):
-    cam = cv2.VideoCapture(1)
-    time.sleep(5)
-    while True:
-        ret_val,img= cam.read()
-        if mirror:
-            img=cv2.flip(img, 1)
-        cv2.imshow('Mywebcam', img)
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
 
-        if cv2.waitKey(1) == 27:
-            break
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+    print(frame)
+else:
+    rval = False
 
-    cv2.destroyAllWindows()
-
-def main():
-    show_webcam(mirror=True)
-
-if __name__ =='__main__':
-    main()
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+cv2.destroyWindow("preview")
